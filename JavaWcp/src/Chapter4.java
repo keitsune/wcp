fe950811727aaeae862f4de3bdd0812e4e159ac7
keitsune <91778786+keitsune@users.noreply.github.com>
2022-04-05 20:37:21 +0900

    Add

---
+++ JavaWcp/src/Chapter4.java
@@ -0,0 +1,12 @@
+
+public class Chapter4 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "KEITA";
+		System.out.println(str);
+		str = "Hello";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+
+	}
+}
