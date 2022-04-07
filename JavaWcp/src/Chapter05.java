0e3aecaee7e2a78cc18d225fe8bdbe11fbccfef2
keitsune <91778786+keitsune@users.noreply.github.com>
2022-04-07 20:54:34 +0900

    Add

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,19 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 44355555422335L;
+		System.out.println(long1);
+		double double1 = 8594859483.85;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		Float float1 = 0.654f;
+		System.out.println(float1);
+		Integer integer1 = 500;
+		System.out.println(integer1);
+		Character character1 = 'こ';
+		System.out.println(character1);
+		int[] array1 = { 45, 30, 55, 23, 53, 49 };
+		System.out.println(array1[3]);
+	}
+}
