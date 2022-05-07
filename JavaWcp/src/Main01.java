340872dd3fd9de0c2ac2f4695d18df35208413df
keitsune <91778786+keitsune@users.noreply.github.com>
2022-05-07 22:34:30 +0900

    Add

---
+++ JavaWcp/src/Main01.java
@@ -0,0 +1,15 @@
+
+public class Main01 {
+	public static void main(String[] args) {
+		int number1 = 3;
+		
+		int number2 = 7;
+		
+		System.out.println(number1 * number2);
+		
+		String text = "プログラミングを学習しよう";
+		
+		System.out.println("progateで" + text);
+ 	}
+
+}
